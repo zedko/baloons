2c77c0a26c39818f62e3858dabd7cace0080d62b
zedko <zedko@bk.ru>
2020-07-26 13:37:39 +0300

    added static, links fixed in html files

--- baloons/baloons/urls.py
+++ baloons/baloons/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import mainapp.views as mainapp
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', mainapp.index),
+    path('cart/', mainapp.cart),
+    path('contacts/', mainapp.contacts),
 ]
+
--- baloons/mainapp/views.py
+++ baloons/mainapp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'mainapp/index.html')
+
+
+def cart(request):
+    return render(request, 'mainapp/cart.html')
+
+
+def contacts(request):
+    return render(request, 'mainapp/contacts.html')
+

23193e38297810f45e5c2f80174757c042fccbeb
zedko <zedko@bk.ru>
2020-08-08 15:11:24 +0300

    reworked xls import to work with categorties, added superuser creation

--- baloons/mainapp/management/commands/xls_import.py
+++ baloons/mainapp/management/commands/xls_import.py
@@ -1,16 +1,27 @@
 from django.core.management import BaseCommand
 from mainapp.models import Product, ProductCategory
+from django.contrib.auth.models import User
 import openpyxl
 
 
-def xls_import(path):
-    """ Provide a path to an xslx file with products - get it imported to the DB """
+def open_xls_file(path):
     try:
         wb = openpyxl.load_workbook(path)
         print(f'file {path} opened')
         ws = wb.active
+        return ws
+    except FileNotFoundError as e:
+        print(e)
+        return e
+
+
+def xls_import_products(path):
+    """ Provide a path to an xslx file with products - get it imported to the DB """
+    ws = open_xls_file(path)
+    if not isinstance(ws, FileNotFoundError):
         for row in ws.iter_rows(min_row=2, values_only=True):
-            category = ProductCategory.objects.filter(id=row[6])[0]
+            category = category_check(row[6])
+            # category = ProductCategory.objects.filter(id=row[6])[0]
             params = {
                       'name': row[0],
                       'image': row[1],
@@ -23,8 +34,17 @@ def xls_import(path):
             print(f'Adding new product: \n {params}')
             new_product = Product(**params)
             new_product.save()
-    except FileNotFoundError as e:
-        print(e)
+
+
+def category_check(category_name: str):
+    """ checks if category is in DB and creates one if not """
+    category = ProductCategory.objects.filter(name=category_name)
+    try:
+        return category[0]
+    except IndexError:
+        new_category = ProductCategory(name=category_name)
+        new_category.save()
+        return new_category
 
 
 class Command(BaseCommand):
@@ -32,5 +52,7 @@ class Command(BaseCommand):
         parser.add_argument("--path", required=True, type=str)
 
     def handle(self, path, **options):
-        xls_import(path)
+        xls_import_products(path)
+        User.objects.create_superuser('django', 'kotnors@mail.ru', 'geekbrains')
+
 
